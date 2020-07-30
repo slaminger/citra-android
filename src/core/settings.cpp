FearlessTobi/citra

audio_core: Add realtime audio

src/core/settings.cpp
@@ -102,6 +102,7 @@ void LogSettings() {
     LogSetting("Audio_EnableDspLleMultithread", Settings::values.enable_dsp_lle_multithread);
     LogSetting("Audio_OutputEngine", Settings::values.sink_id);
     LogSetting("Audio_EnableAudioStretching", Settings::values.enable_audio_stretching);
+    LogSetting("Audio_EnableRealTime", Settings::values.enable_realtime_audio);
     LogSetting("Audio_OutputDevice", Settings::values.audio_device_id);
     LogSetting("Audio_InputDeviceType", static_cast<int>(Settings::values.mic_input_type));
     LogSetting("Audio_InputDevice", Settings::values.mic_input_device);

slaminger/citra-android

src/core/settings.cpp
@@ -106,6 +106,7 @@ void LogSettings() {
     log_setting("Audio_OutputDevice", values.audio_device_id);
     log_setting("Audio_InputDeviceType", static_cast<int>(values.mic_input_type));
     log_setting("Audio_InputDevice", values.mic_input_device);
+    log_setting("Audio_EnableRealTime", Settings::values.enable_realtime_audio);
     using namespace Service::CAM;
     log_setting("Camera_OuterRightName", values.camera_name[OuterRightCamera]);
     log_setting("Camera_OuterRightConfig", values.camera_config[OuterRightCamera]);